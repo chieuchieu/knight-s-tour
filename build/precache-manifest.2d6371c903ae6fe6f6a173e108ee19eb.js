8b1cb741ccd22d7897e66048cc2ce6836ea2799c
Chiêu <39668225+chieuchieu@users.noreply.github.com>
2019-08-14 00:41:51 +0700

    Add files via upload

---
+++ build/precache-manifest.2d6371c903ae6fe6f6a173e108ee19eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3651111e8b52a365cff8f9308043fe3c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44e8b067c48b57233ada",
+    "url": "./static/css/main.1ad22a5e.chunk.css"
+  },
+  {
+    "revision": "0debf52d5a42020e34f8",
+    "url": "./static/js/2.3b194e55.chunk.js"
+  },
+  {
+    "revision": "44e8b067c48b57233ada",
+    "url": "./static/js/main.15a71074.chunk.js"
+  },
+  {
+    "revision": "77323e70069941a799a5",
+    "url": "./static/js/runtime~main.1fa64475.js"
+  }
+]);
